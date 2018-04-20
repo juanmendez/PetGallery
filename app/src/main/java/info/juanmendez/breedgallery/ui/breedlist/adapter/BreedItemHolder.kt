54ab13aa66a1e6f47dd0830f941302f363386f57
Juan Mendez <contact@juanmendez.info>
2018-04-20 14:22:13 -0500

    item_pet.xml renamed

--- app/src/main/java/info/juanmendez/breedgallery/ui/breedlist/adapter/BreedItemHolder.kt
+++ app/src/main/java/info/juanmendez/breedgallery/ui/breedlist/adapter/BreedItemHolder.kt
@@ -1,17 +1,16 @@
 package info.juanmendez.breedgallery.ui.breedlist.adapter
 
 import android.support.v7.widget.RecyclerView
-import com.android.databinding.library.baseAdapters.BR
+import info.juanmendez.breedgallery.BR
 import info.juanmendez.breedgallery.data.api.BreedCall
 import info.juanmendez.breedgallery.data.api.BreedClientHttp
 import info.juanmendez.breedgallery.data.api.models.Breed
-import info.juanmendez.breedgallery.databinding.ViewPetItemBinding
-import info.juanmendez.breedgallery.ui.breedlist.BreedListActivity
+import info.juanmendez.breedgallery.databinding.ItemPetBinding
 import info.juanmendez.breedgallery.ui.breedlist.BreedListContract
 import info.juanmendez.breedgallery.ui.breedlist.viewmodel.BreedObservable
 import javax.inject.Inject
 
-class BreedItemHolder( var view: BreedListContract.View, var binding: ViewPetItemBinding) : RecyclerView.ViewHolder(binding.root) {
+class BreedItemHolder( var view: BreedListContract.View, var binding: ItemPetBinding) : RecyclerView.ViewHolder(binding.root) {
     @Inject
     lateinit var breedClientHttp: BreedClientHttp
 
@@ -21,7 +20,6 @@ class BreedItemHolder( var view: BreedListContract.View, var binding: ViewPetIte
     }
 
     fun setBreed(breed: Breed) {
-
         binding.breedObservable?.breed = breed
         if(breed.pictureList.isEmpty()) {
             breedClientHttp.getPicsByBreed(breed.name, object : BreedCall<List<String>> {
--- app/src/main/java/info/juanmendez/breedgallery/ui/breedlist/adapter/BreedListAdapter.kt
+++ app/src/main/java/info/juanmendez/breedgallery/ui/breedlist/adapter/BreedListAdapter.kt
@@ -8,39 +8,34 @@ import android.databinding.Observable.OnPropertyChangedCallback
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import com.android.databinding.library.baseAdapters.BR
-import info.juanmendez.breedgallery.databinding.ViewPetItemBinding
+import info.juanmendez.breedgallery.BR
+import info.juanmendez.breedgallery.databinding.ItemPetBinding
 import info.juanmendez.breedgallery.ui.breedlist.BreedListContract.View
 
-/**
- * Created by Juan Mendez on 2/14/18.
- * Through dataBinding the recyclerView can have a binding reference and refresh its content
- * upon changes triggered at breadListObservable.breedList
- */
 class BreedListAdapter(private val inflater: LayoutInflater, private val view: View) : RecyclerView.Adapter<BreedItemHolder>(), LifecycleObserver {
-    private val mObservable = view.getBreadListObservable()
-    private lateinit var mCallBack: OnPropertyChangedCallback
+    private val listObservable = view.getBreadListObservable()
+    private lateinit var callback: OnPropertyChangedCallback
 
     init {
         view.getLifeCycle().addObserver(this)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): BreedItemHolder {
-        var binding = ViewPetItemBinding.inflate(inflater, parent, false)
-        binding.breedListObservable = mObservable
+        var binding = ItemPetBinding.inflate(inflater, parent, false)
+        binding.breedListObservable = listObservable
         return BreedItemHolder(view, binding)
     }
 
-    override fun getItemCount(): Int = mObservable.breedList.size
+    override fun getItemCount(): Int = listObservable.breedList.size
 
     override fun onBindViewHolder(holder: BreedItemHolder?, position: Int) {
-        holder?.setBreed(mObservable.breedList[position])
+        holder?.setBreed(listObservable.breedList[position])
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResume() {
 
-        mCallBack = object : OnPropertyChangedCallback() {
+        callback = object : OnPropertyChangedCallback() {
             override fun onPropertyChanged(observable: Observable?, br: Int) {
                 if(br == BR.breedList) {
                     notifyDataSetChanged()
@@ -48,11 +43,11 @@ class BreedListAdapter(private val inflater: LayoutInflater, private val view: V
             }
         }
 
-        mObservable.addOnPropertyChangedCallback(mCallBack)
+        listObservable.addOnPropertyChangedCallback(callback)
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPause() {
-        mObservable.removeOnPropertyChangedCallback(mCallBack)
+        listObservable.removeOnPropertyChangedCallback(callback)
     }
 }

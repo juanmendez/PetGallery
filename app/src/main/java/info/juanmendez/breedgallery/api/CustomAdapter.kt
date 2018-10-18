bb108ca4648466c66eebc437884388f9875dc9f7
Juan Mendez <contact@juanmendez.info>
2018-10-17 22:09:13 -0500

    qualifiers

--- app/src/main/java/info/juanmendez/breedgallery/api/CustomAdapter.kt
+++ app/src/main/java/info/juanmendez/breedgallery/api/CustomAdapter.kt
@@ -1,18 +1,35 @@
 package info.juanmendez.breedgallery.api
 
 import com.squareup.moshi.FromJson
+import com.squareup.moshi.JsonQualifier
+import com.squareup.moshi.ToJson
 import info.juanmendez.breedgallery.models.Breed
-import info.juanmendez.breedgallery.models.BreedListResponse
-import info.juanmendez.breedgallery.models.RawBreedListResponse
 
 class CustomAdapter {
     @FromJson
-    fun toBreedListResponse(raw: RawBreedListResponse): BreedListResponse {
+    @BreedList
+    fun breedsFromJson(map: Map<String, List<String>?>): List<Breed> {
         val breeds = mutableListOf<Breed>()
-        raw.message.forEach {
+
+        map.forEach {
             breeds.add(Breed(it.key, it.value))
         }
 
-        return BreedListResponse(raw.status, breeds.toList())
+        return breeds
+    }
+
+    @ToJson
+    fun breedsToJson(@BreedList breeds: List<Breed>): Map<String, List<String>?> {
+        val map = mutableMapOf<String, List<String>?>()
+
+        breeds.forEach {
+            map.put(it.name, it.subBreed)
+        }
+
+        return map
     }
 }
+
+@Retention(AnnotationRetention.RUNTIME)
+@JsonQualifier
+annotation class BreedList
--- app/src/main/java/info/juanmendez/breedgallery/models/BreedListResponse.kt
+++ app/src/main/java/info/juanmendez/breedgallery/models/BreedListResponse.kt
@@ -1,5 +1,8 @@
 package info.juanmendez.breedgallery.models
 
-data class BreedListResponse(var status: String, var list: List<Breed>)
+import com.squareup.moshi.Json
+import info.juanmendez.breedgallery.api.BreedList
+
+data class BreedListResponse(var status: String, @BreedList @Json(name = "message") var list: List<Breed>)
 
 data class RawBreedListResponse(var status: String, var message: Map<String, List<String>>)
